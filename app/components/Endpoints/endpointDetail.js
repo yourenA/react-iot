1e928c3a35774e241bfc6d9246e0fd91919faa2c
yourena <2373638339@qq.com>
2017-03-21 18:01:39 +0800

    修改设备域detail布局

--- app/components/Endpoints/endpointDetail.js
+++ app/components/Endpoints/endpointDetail.js
@@ -10,13 +10,57 @@ import axios from 'axios';
 import messageJson from './../../common/message.json';
 import {getHeader} from './../../common/common.js';
 import './index.scss'
-
+import AddDetailForm from './addDetailForm.js'
+const datasource=[{
+    uuid:'daxpopd-fa-ff00',
+    name:'temp01',
+    user:'user01',
+    desc:'desc',
+    status:1,
+    type:2,
+    last_on_time:'2017-3-21 16:32:45',
+    create_time:'2017-3-21 16:32:45',
+    meta:{
+        group_name:'toyota',
+        desc:'desc',
+        strategy:'strategy',
+        theme:[{
+            authority:'0',
+            theme_content:'endpoints/node-001/temp/action'
+        },{
+            authority:'2',
+            theme_content:'endpoints/node-001/temp/action'
+        }]
+    }
+},
+    {
+        uuid:'daxpopd-fa-f56f00',
+        name:'temp01',
+        user:'user01',
+        desc:'desc',
+        status:1,
+        type:2,
+        last_on_time:'2017-3-21 16:32:45',
+        create_time:'2017-3-21 16:32:45',
+        meta:{
+            group_name:'toyota',
+            desc:'desc',
+            strategy:'strategy',
+            theme:[{
+                authority:'0',
+                theme_content:'endpoints/node-001/temp/action'
+            },{
+                authority:'2',
+                theme_content:'endpoints/node-001/temp/action'
+            }]
+        }
+    }]
 class EndPointDetail extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data:[],
-            loaded:false,
+            data:datasource,
+            loaded:true,
             q:'',
             page:0,
             meta:{pagination:{total:0,per_page:0}},
@@ -95,43 +139,33 @@ class EndPointDetail extends Component {
         const {data, page, q,meta,loaded} = this.state;
         console.log(meta)
         const columns = [{
-            title: '域名称',
+            title: '设备名称',
             dataIndex: 'name',
             key: 'name',
-            render: (text, record, index) => {
-                return (
-                    <Link to={'/basic/endpoints/'+record.uuid} title={text}>{text}</Link>
-
-                )
-            }
         },{
-            title: '描述',
-            dataIndex: 'description',
-            key: 'description',
-            render: (text, record, index) => {
-                return (
-                    <div className="line-clamp3 line-edit">
-                        <span title={text}>{text}</span><Icon type="edit" onClick={this.showEditDesc.bind(this,record.uuid,record.name,record.description)}/>
-                    </div>
-
-                )
-            }
+            title: '用户名',
+            dataIndex: 'user',
+            key: 'user'
         }, {
-            title: '接入地址',
-            dataIndex: 'websocket_hostname',
-            key: 'websocket_hostname',
+            title: '描述',
+            dataIndex: 'desc',
+            key: 'desc',
         },  {
-            title: '设备总数',
-            dataIndex: 'device_count',
-            key: 'device_count',
+            title: '类型',
+            dataIndex: 'type',
+            key: 'type',
         },  {
-            title: '在线设备数',
-            dataIndex: 'device_online_count',
-            key: 'device_online_count',
+            title: '状态',
+            dataIndex: 'status',
+            key: 'status',
+        },{
+            title: '最后在线时间',
+            dataIndex: 'last_on_time',
+            key: 'last_on_time',
         },{
             title: '创建时间',
-            dataIndex: 'created_at',
-            key: 'created_at',
+            dataIndex: 'create_time',
+            key: 'create_time',
         }, {
             title: '操作',
             key: 'action',
@@ -149,6 +183,58 @@ class EndPointDetail extends Component {
                 )
             }
         }];
+        const expandedRowRender=(record)=>{
+            const columns=[{
+                title: '权限',
+                dataIndex: 'authority',
+                key: 'authority',
+            },{
+                title: '主题',
+                dataIndex: 'theme_content',
+                key: 'theme_content',
+            },];
+
+            return (
+                <div className="expandRowRender-box">
+                    <div className="expandRowRender-table">
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td>设备组</td>
+                                <td>{record.meta.group_name}</td>
+                            </tr>
+                            <tr>
+                                <td>描述</td>
+                                <td>{record.meta.desc}</td>
+                            </tr>
+                            <tr>
+                                <td>策略</td>
+                                <td>{record.meta.strategy}</td>
+                            </tr>
+                            <tr>
+                                <td>主题</td>
+                                <td><Table
+                                    style={{width:'300px'}}
+                                    size="small"
+                                    rowKey="authority"
+                                    columns={columns}
+                                    dataSource={record.meta.theme}
+                                    pagination={false}
+                                /></td>
+                            </tr>
+                            </tbody>
+
+                        </table>
+                    </div>
+                    <div className="expandRowRender-operate">
+                        <Button type="primary">连通测试</Button> <span className="ant-divider" />
+                        <Button type="primary">修改</Button> <span className="ant-divider" />
+                        <Button type="primary">重新生成秘钥</Button>
+                    </div>
+                </div>
+
+            );
+        }
         return (
             <div className="Home">
                 <Row>
@@ -168,7 +254,7 @@ class EndPointDetail extends Component {
                             <Button className="search-btn" type="primary" icon="plus" onClick={()=>{this.setState({addModal:true})}}>创建域</Button>
                         </div>
                         <Loading show={loaded} />
-                        <Table bordered  style={{display:loaded? 'block':'none'}} rowKey="uuid" columns={columns} dataSource={data} pagination={false}/>
+                        <Table bordered expandedRowRender={(record)=>expandedRowRender(record ) } style={{display:loaded? 'block':'none'}} rowKey="uuid" columns={columns} dataSource={data} pagination={false}/>
                         <Pagination total={meta.pagination.total  } current={page} pageSize={meta.pagination.per_page}
                                     style={{marginTop: '10px'}} onChange={this.onPageChange}/>
 
@@ -176,19 +262,9 @@ class EndPointDetail extends Component {
                     <Modal
                         visible={this.state.addModal}
                         title="创建新域"
-                        onOk={this.handleOk}
                         onCancel={()=>{this.setState({addModal:false})}}
-                        footer={[
-                            <Button key="back" type="ghost" size="large"
-                                    onClick={()=>{this.setState({addModal:false})}}>取消</Button>,
-                            <Button key="submit" type="primary" size="large" onClick={this.addEndPoint}>
-                                确定
-                            </Button>,
-                        ]}
                     >
-                        <Input style={{marginBottom:'15px'}} onChange={this.changeEndpointName} value={this.state.addEndpointName} placeholder="名称:长度3-32个字符" />
-                        <Input  onChange={this.changeEndpointDesc} value={this.state.addEndpointDesc} type="textarea" placeholder="描述" autosize={{ minRows: 2, maxRows: 6 }} />
-                        <p>说明：名称只能由英文字母、数字、“_”(下划线)、“-”（即中横线）构成。“-” 不能单独或连续使用，不能放在开头或结尾。</p>
+                        <AddDetailForm />
                     </Modal>
                 </Row>
             </div>
